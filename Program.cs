039aded6e127a0e62b25b2045941b9eaf8e533ac
Alik <9774952689@bk.ru>
2022-09-04 01:43:00 +0300

    CompletedELKA

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите номер вагона в который заходите: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер вагона который видите: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a  < b)
+{
+    int c = a+b-1;
+    Console.WriteLine("В электричке " + c + " вагонов");
+}
+else
+{
+Console.WriteLine("Подсчитатьколичество невозможно)");
+}
